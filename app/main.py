bbb499c83f8f7310f6816958faad0c7aa21eed5b
amarnath <amaramarnath2000@gmail.com>
2022-12-22 20:46:10 +0530

    Remove print

--- app/main.py
+++ app/main.py
@@ -19,9 +19,6 @@ def main():
         print("Expected first argument to be '-E'")
         exit(1)
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
