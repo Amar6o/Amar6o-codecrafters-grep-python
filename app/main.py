82c1c0b5ba7e07643f3f8fd211096975c968f1b6
amarnath <amaramarnath2000@gmail.com>
2022-12-28 19:56:30 +0530

    positive character group

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def match_pattern(input_line, pattern):
     elif pattern == "\d":
         return any(c.isdigit() for c in input_line)
     elif pattern.startswith("["):
-        c_in_positive_c_group = pattern.split(']')[0],[1]
+        c_in_positive_c_group = pattern.split(']')[0],[1:]
         return any(c in c_in_positive_c_group for c in input_line)
     elif pattern == "\w":
         return any(c.isalnum() for c in input_line)
