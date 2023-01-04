e4d3bc12deb26805e2e2b5521af3397ef0290985
amarnath <amaramarnath2000@gmail.com>
2023-01-04 12:11:10 +0530

    combining character class

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ def match_pattern(input_line, pattern):
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
-    input_line = sys.stdin.read().splitlines()[0]
+    input_line = sys.stdin.readline().splitlines()[0]
 
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
